7eeed673a6bd7e3140d03cdc66d8259621f14287
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2022-08-02 09:42:40 +0300

    [ADD] Add sectionTitle to replace sectionTitre

---
+++ src/components/sectionTitle.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const SectionTitle = (props : any) => {
+    return (
+        <>
+            <h1 className="section-titre" style={{color: "#222"}}>  
+                {props.text}
+                <hr style={{width: "20%", marginTop: 10,  border: "1px solid #edbb28"}}/>
+            </h1>
+        </>
+    )
+}
+
+
+export default SectionTitle;
+
